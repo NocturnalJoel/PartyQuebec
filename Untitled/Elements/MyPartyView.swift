00fdedf3205c02188f8aac697722e2c6c951f9ce
NocturnalJoel <jlacostetherrien@gmail.com>
2024-05-03 20:43:51 -0400

    mieux

---
+++ Untitled/Elements/MyPartyView.swift
@@ -0,0 +1,18 @@
+//
+//  MyPartyView.swift
+//  Untitled
+//
+//  Created by Joël Lacoste-Therrien on 2024-05-02.
+//
+
+import SwiftUI
+
+struct MyPartyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyPartyView()
+}
---
+++ Untitled/Elements/TicketView.swift
@@ -0,0 +1,18 @@
+//
+//  TicketView.swift
+//  Untitled
+//
+//  Created by Joël Lacoste-Therrien on 2024-05-02.
+//
+
+import SwiftUI
+
+struct TicketView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TicketView()
+}
